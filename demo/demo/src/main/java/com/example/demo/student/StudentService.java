80755333ac4878131d32e8ca879676f92dfd78b7
Abubakar <220176639@aston.ac.uk>
2023-10-30 15:53:51 +0000

    spring framework student class

---
+++ demo/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+@Autowired
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+    public List<Student> getStudent(){
+            return studentRepository.findAll();
+        }
+}
