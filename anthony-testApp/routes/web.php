ad30234284df9a110f79469437203ca2330e1045
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-11-25 22:38:54 +0000

    route change

--- anthony-testApp/routes/web.php
+++ anthony-testApp/routes/web.php
@@ -26,6 +26,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/', function () {
+        return view('FrontEnd/product');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
