5780226f655633f80b9b292f9fe10e7610773b87
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-11-25 22:45:25 +0000

    route changes

--- anthony-testApp/resources/views/FrontEnd/master.blade.php
+++ anthony-testApp/resources/views/FrontEnd/master.blade.php
@@ -12,36 +12,51 @@
         />
     </head>
     <body>
-        <!--             Header                     -->
-        @yield ('header')
-        <header>
-            
-        <section class="nav-header">
-        
-            <a href="" class="logo"> <img src="{{asset('assets/images/Screenshot_2023-11-16_030651.png')}}" alt=""></a>
-            
+        <!--             Header                      -->
 
+        <header>
+
+        <section class="nav-header" >
+
+
+            <a href="" class="logo"> <img src="{{asset('assets/images/Screenshot_2023-11-16_030651.png')}}" alt=""></a>
             <input type="checkbox" id="check">
             <label for="check" class="menu-icon">
             <i class="bx bx-menu" id="menu"></i>
             <i class="bx bx-x" id="close"></i>
             </label>
-
             <nav class="navbar">
             <a href="#">Home</a>
             <a href="#">Products</a>
             <a href="#">Contact Us</a>
-            <a href="#" class="login-text"><i class="bx bx-user"></i> Log in</a>
+
+                   <!--        Fixed the heading so that the login and register is included in the header           -->
+
+                @if (Route::has('login'))
+
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                        @else
+                            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"><i class="bx bx-user"></i> Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                            @endif
+                        @endauth
+                @endif
+
+            <!--<a href="#" class="login-text"><i class="bx bx-user"></i> Log in</a>  !-->
             <a href="#" class="cart-icon"
-                ><i class="bx bx-shopping-bag"></i> Basket</a
-            >
+                ><i class="bx bx-shopping-bag"></i> Basket</a>
             </nav>
         </section>
+
+
         <!--         Hero Section         -->
         <section id="hero">
             <div class="container">
             <div class="hero-info">
-        
+
                 <h1>Explore Top-Quality Gaming Gear</h1>
                 <p class="hero-text">
                 Immerse yourself in the world of gaming with Valhalla's collection of top-notch gaming laptops.
@@ -64,7 +79,7 @@
         </div>
         </div>
     </header>
-  
+
         <!-- Best seller prodcuts-->
         <section class= "main">
         <section id="best-seller-sction">
@@ -94,14 +109,14 @@
                     <div class="laptop">
                         <img src="{{asset('assets/images/GU603VI-N4015W_1_Supersize.jpg')}}"   alt="laptop 3">
                             <h1>Asus TUF ZQ13X</h1>
-                        
+
                             <p>AMD Ryzen 7</p>
                             <p>RAM: 32GB</p>
                             <p> 1TB SSD</p>
                             <p>Graphics: AMD Radeon RX 5700</p>
                             <a href="#" class="buy-product">Add to Basket</a>
                     </div>
-                
+
                 </div>
             </section>
 
@@ -111,10 +126,10 @@
         <div class="box-card">
         <div class = "info">
             <h1 class="section-heading">High-Performance Gaming Laptops</h1>
-            <p>Our high-performance gaming laptops are packed with the latest hardware, 
+            <p>Our high-performance gaming laptops are packed with the latest hardware,
             including powerful processors, dedicated graphics cards, and high-refresh-rate displays. </p>
             <p>They can handle even the most demanding games with ease, and they're also great for other graphics-intensive tasks
-            like video editing, 3D modelling and game development.</p> 
+            like video editing, 3D modelling and game development.</p>
             <a href="#" class="view-laptops-btn">View Products</a>
         </div>
         <div>
@@ -127,7 +142,7 @@
     </section>
                                 <!-- Our Product Section-->
     <section class= "main">
-        
+
     <section id="best-seller-sction">
         <div class="big-card">
         <h1 class = "title-sellers">Our Laptops</h1>
@@ -135,7 +150,7 @@
             <div id="best-seller-container">
                 <div class="laptop">
                     <img src="{{asset('assets/images/laptop1.jpg')}}" alt="laptop 1">
-                     
+
                         <h1>MSI Raider GE78HX</h1>
                         <p>Processor: Intel Core i7</p>
                         <p>RAM: 16GB</p>
@@ -155,18 +170,18 @@
                 <div class="laptop">
                     <img src="{{asset('assets/images/30035515_1_Supersize.png')}}"   alt="laptop 3">
                         <h1>Asus TUF ZQ13X</h1>
-                    
+
                         <p>AMD Ryzen 7</p>
                         <p>RAM: 32GB</p>
                         <p> 1TB SSD</p>
                         <p>Graphics: AMD Radeon RX 5700</p>
                         <a href="#" class="buy-product">Add to Basket</a>
                 </div>
-                
-                
+
+
             </div>
             <div class="big-card">
-                
+
             <!-- Add this line -->
                     <div id="best-seller-container">
                         <div class="laptop">
@@ -190,21 +205,20 @@
                         <div class="laptop">
                             <img src="{{asset('assets/images/30035515_1_Supersize.png')}}"   alt="laptop 6">
                                 <h1>Asus TUF ZQ13X</h1>
-                            
+
                                 <p>AMD Ryzen 7</p>
                                 <p>RAM: 32GB</p>
                                 <p> 1TB SSD</p>
                                 <p>Graphics: AMD Radeon RX 5700</p>
                                 <a href="#" class="buy-product">Add to Basket</a>
                         </div>
-                    
-                        
+
+
                     </div>
         </section>
 
 
-        @yield('footer')
-    
+      @include('footer')
 
     </body>
     </html>
