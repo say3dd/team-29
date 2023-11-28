0ebae60c8bdc9cab973ebc8335f719b55fe8ff89
Anthony <147273551+AnthonyResuello@users.noreply.github.com>
2023-11-28 15:06:35 +0000

    Improved design on product section

--- anthony-testApp/resources/views/FrontEnd/master.blade.php
+++ anthony-testApp/resources/views/FrontEnd/master.blade.php
@@ -95,32 +95,41 @@
                     <div class="title-line"></div> <!-- Add this line -->
                     <div id="best-seller-container">
                         <div class="laptop">
-                            <img src="{{ asset('assets/images/laptop1.jpg') }}" alt="laptop 1">
-                            <h1>MSI Raider GE78HX</h1>
-                            <p>Processor: Intel Core i7</p>
-                            <p>RAM: 16GB</p>
-                            <p>Storage: 512GB SSD</p>
-                            <p>Graphics: NVIDIA RTX 3080</p>
+                            <div class = "specs-container">
+                                <img src="{{ asset('assets/images/GU603VI-N4015W_1_Supersize.jpg') }}" alt="laptop 4">
+                          
+                                <h1>MSI Raider GE78HX</h1>
+                                <p>Processor:AMD RYZEN 5950X</p>
+                                <p>RAM: 16GB</p>
+                                <p>Graphics: NVIDIA RTX 3080</p>
+                                <h3>£420</h3>
+                            </div>
+            
                             <a href="#" class="buy-product">Add to Basket</a>
                         </div>
                         <div class="laptop">
-
-                            <img src="{{ asset('assets/images/GU603VI-N4015W_1_Supersize.jpg') }}" alt="laptop 2">
-                            <h1>MSI M100 32FS</h1>
-                            <p>Processor: Intel Core i7</p>
-                            <p>RAM: 16GB</p>
-                            <p>Storage: 512GB SSD</p>
-                            <p>Graphics: NVIDIA RTX 2060</p>
+                            <div class = "specs-container">
+                                <img src="{{ asset('assets/images/30035515_1_Supersize.png') }}" alt="laptop 4">
+                                <h1>MSI Raider GE78HX</h1>
+                                <p>Processor: Intel Core i7</p>
+                                <p>RAM: 16GB</p>
+                                <p>Graphics: NVIDIA RTX 3080</p>
+                                <h3>£590</h3>
+                            </div>
+            
                             <a href="#" class="buy-product">Add to Basket</a>
                         </div>
                         <div class="laptop">
-                            <img src="{{ asset('assets/images/GU603VI-N4015W_1_Supersize.jpg') }}" alt="laptop 3">
-                            <h1>Asus TUF ZQ13X</h1>
-
-                            <p>AMD Ryzen 7</p>
-                            <p>RAM: 32GB</p>
-                            <p> 1TB SSD</p>
-                            <p>Graphics: AMD Radeon RX 5700</p>
+                            <div class = "specs-container">
+                            <img src="{{ asset('assets/images/laptop1.jpg') }}" alt="laptop 4">
+                          
+                                <h1>MSI Raider GE78HX</h1>
+                                <p>Processor: AMD 5800X</p>
+                                <p>RAM: 16GB</p>
+                                <p>Graphics: NVIDIA RTX 3080</p>
+                                <h3>£810</h3>
+                            </div>
+            
                             <a href="#" class="buy-product">Add to Basket</a>
                         </div>
 
@@ -158,72 +167,92 @@
                         <div class="title-line"></div> <!-- Add this line -->
                         <div id="best-seller-container">
                             <div class="laptop">
-                                <img src="{{ asset('assets/images/laptop1.jpg') }}" alt="laptop 1">
-
-                                <h1>MSI Raider GE78HX</h1>
-                                <p>Processor: Intel Core i7</p>
-                                <p>RAM: 16GB</p>
-                                <p>Storage: 512GB SSD</p>
-                                <p>Graphics: NVIDIA RTX 3080</p>
+                                <div class = "specs-container">
+                                    <img src="{{ asset('assets/images/GU603VI-N4015W_1_Supersize.jpg') }}" alt="laptop 4">
+                              
+                                    <h1>MSI Raider GE78HX</h1>
+                                    <p>Processor:AMD RYZEN 5950X</p>
+                                    <p>RAM: 16GB</p>
+                                    <p>Graphics: NVIDIA RTX 3080</p>
+                                    <h3>£420</h3>
+                                </div>
+                
+                                <a href="#" class="buy-product">Add to Basket</a>
+                            </div>
+                           <div class="laptop">
+                                <div class = "specs-container">
+                                    <img src="{{ asset('assets/images/30035515_1_Supersize.png') }}" alt="laptop 4">
+                                    <h1>MSI Raider GE78HX</h1>
+                                    <p>Processor: Intel Core i7</p>
+                                    <p>RAM: 16GB</p>
+                                    <p>Graphics: NVIDIA RTX 3080</p>
+                                    <h3>£590</h3>
+                                </div>
+                
                                 <a href="#" class="buy-product">Add to Basket</a>
                             </div>
                             <div class="laptop">
-                                <img src="{{ asset('assets/images/GU603VI-N4015W_1_Supersize.jpg') }}" alt="laptop 2">
-                                <h1>MSI M100 32FS</h1>
-                                <p>Processor: Intel Core i7</p>
-                                <p>RAM: 16GB</p>
-                                <p>Storage: 512GB SSD</p>
-                                <p>Graphics: NVIDIA RTX 2060</p>
+                                <div class = "specs-container">
+                                <img src="{{ asset('assets/images/laptop1.jpg') }}" alt="laptop 4">
+                              
+                                    <h1>MSI Raider GE78HX</h1>
+                                    <p>Processor: AMD 5800X</p>
+                                    <p>RAM: 16GB</p>
+                                    <p>Graphics: NVIDIA RTX 3080</p>
+                                    <h3>£810</h3>
+                                </div>
+                
                                 <a href="#" class="buy-product">Add to Basket</a>
                             </div>
-                            <div class="laptop">
-                                <img src="{{ asset('assets/images/30035515_1_Supersize.png') }}" alt="laptop 3">
-                                <h1>Asus TUF ZQ13X</h1>
-
-                                <p>AMD Ryzen 7</p>
-                                <p>RAM: 32GB</p>
-                                <p> 1TB SSD</p>
-                                <p>Graphics: AMD Radeon RX 5700</p>
-                                <a href="#" class="buy-product">Add to Basket</a>
-                            </div>
+                
+                        </div>
+            
 
 
                         </div>
                         <div class="big-card">
 
                             <!-- Add this line -->
-                            <div id="best-seller-container">
+                                
                                 <div class="laptop">
+                                    <div class = "specs-container">
                                     <img src="{{ asset('assets/images/laptop1.jpg') }}" alt="laptop 4">
-                                    <h1>MSI Raider GE78HX</h1>
-                                    <p>Processor: Intel Core i7</p>
-                                    <p>RAM: 16GB</p>
-                                    <p>Storage: 512GB SSD</p>
-                                    <p>Graphics: NVIDIA RTX 3080</p>
+                                  
+                                        <h1>MSI Raider GE78HX</h1>
+                                        <p>Processor: Intel Core i7</p>
+                                        <p>RAM: 16GB</p>
+                                        <p>Graphics: NVIDIA RTX 3080</p>
+                                        <h3>£450</h3>
+                                    </div>
+                    
                                     <a href="#" class="buy-product">Add to Basket</a>
                                 </div>
                                 <div class="laptop">
-                                    <img src="{{ asset('assets/images/GU603VI-N4015W_1_Supersize.jpg') }}"
-                                        alt="laptop 5">
-                                    <h1>MSI M100 32FS</h1>
-                                    <p>Processor: Intel Core i7</p>
-                                    <p>RAM: 16GB</p>
-                                    <p>Storage: 512GB SSD</p>
-                                    <p>Graphics: NVIDIA RTX 2060</p>
+                                    <div class = "specs-container">
+                                    <img src="{{ asset('assets/images/30035515_1_Supersize.png') }}" alt="laptop 4">
+                                  
+                                        <h1>MSI Raider GE78HX</h1>
+                                        <p>Processor: Intel Core i9</p>
+                                        <p>RAM: 8GB</p>
+                                        <p>Graphics: NVIDIA RTX 2060</p>
+                                        <h3>£700</h3>
+                                    </div>
+                    
                                     <a href="#" class="buy-product">Add to Basket</a>
                                 </div>
                                 <div class="laptop">
-                                    <img src="{{ asset('assets/images/30035515_1_Supersize.png') }}" alt="laptop 6">
-                                    <h1>Asus TUF ZQ13X</h1>
-
-                                    <p>AMD Ryzen 7</p>
-                                    <p>RAM: 32GB</p>
-                                    <p> 1TB SSD</p>
-                                    <p>Graphics: AMD Radeon RX 5700</p>
+                                    <div class = "specs-container">
+                                    <img src="{{ asset('assets/images/GU603VI-N4015W_1_Supersize.jpg') }}" alt="laptop 4">
+                                  
+                                        <h1>MSI Raider GE78HX</h1>
+                                        <p>Processor: Intel Core i7</p>
+                                        <p>RAM: 16GB</p>
+                                        <p>Graphics: NVIDIA RTX 3080</p>
+                                        <h3>£650</h3>
+                                    </div>
+                    
                                     <a href="#" class="buy-product">Add to Basket</a>
                                 </div>
-
-
                             </div>
                 </section>
 
