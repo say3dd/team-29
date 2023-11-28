f7258098fab80e1640f5cb5dae9edbb8372c9211
Anthony <147273551+AnthonyResuello@users.noreply.github.com>
2023-11-28 13:59:50 +0000

    Improved Footer

--- anthony-testApp/resources/views/FrontEnd/master.blade.php
+++ anthony-testApp/resources/views/FrontEnd/master.blade.php
@@ -232,12 +232,47 @@
 
   <footer>
     @yield('footer')
-    <div class="footer-content">
-      <p> Made By Valhalla &copy; </p>
-      <div class="social-icons">
-        </div>
-    </div>
+    
+    <section class="footer-section">
+        <footer>
+            <div class="footer-content">
+                <div class="footer-links">
+                    <div class="footer-column">
+                       
+                        <ul>
+                            <li><a href="#">Our Story</a></li>
+                            <li><a href="#">Mission & Vision</a></li>
+                            <li><a href="#">Team</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-column">
+                        
+                        <ul>
+                            <li><a href="#">Laptops</a></li>
+                            <li><a href="#">Keyboards</a></li>
+                            <li><a href="#">Mice</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-column">
+                       
+                        <ul>
+                            <li><a href="#">Support</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Customer Service</a></li>
+                        </ul>
+                    </div>
+                </div>
+              
+            </div>
+            <div class="footer-bottom">
+                <p>&copy; 2023 Valhalla</p>
+            </div>
+        </footer>
+    </section>
+
+
   </footer>
+  
 </section>
 
     </body>
