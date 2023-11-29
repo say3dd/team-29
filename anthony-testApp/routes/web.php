7e8aaba4e2d4b810b98077f644488046fd2247d7
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-11-29 16:34:47 +0000

    route

--- anthony-testApp/routes/web.php
+++ anthony-testApp/routes/web.php
@@ -36,7 +36,7 @@ Route::get('/test', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/home', [HomeController::class,'index']);
+Route::get('/home', [HomeController::class,'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
