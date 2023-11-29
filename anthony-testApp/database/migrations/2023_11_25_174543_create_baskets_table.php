e32e48f58b9df049978bf99188acb13955bdb011
noramknarf <45110161+noramknarf@users.noreply.github.com>
2023-11-29 10:01:56 +0000

    Added basic DB skeleton/functionality for baskets

---
+++ anthony-testApp/database/migrations/2023_11_25_174543_create_baskets_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * This migration is to create a table to hold user's baskets.
+ * 
+ * In theory, each basket should link to a user, with users
+ * using their ID as a foreign key 
+ * (I'm not super well-versed on dbms so might need to revise that)
+ * 
+ * the idea is that when the User model is created in 
+ * RegisteredUserController->store(), a basket will be made for them
+ * 
+ * It really wasn't clear what people want to do so I just kinda did this cause I /need/ to do something
+ * hope you don't mind
+ */
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('baskets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('baskets');
+    }
+};
