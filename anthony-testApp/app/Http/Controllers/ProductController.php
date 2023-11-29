07adda011735c344f95d015de0a6fa4919e30cba
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-11-29 16:32:39 +0000

    route added

--- anthony-testApp/app/Http/Controllers/ProductController.php
+++ anthony-testApp/app/Http/Controllers/ProductController.php
@@ -14,10 +14,10 @@ class ProductController extends Controller
     }
 
 
-    //directing user the product page
-    // public function index(){
-    //     if(Auth::user()){ 
-    //         return view('FrontEnd.product');
-    //     }
+    // directing user the product page
+    public function index(){
+        if(Auth::user()){ 
+            return view('FrontEnd.product');
+        }
     }
 }
