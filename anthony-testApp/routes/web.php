b9f3c432d923bb6ce9a13694fa5becb37004217a
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-11-29 10:11:37 +0000

    product

--- anthony-testApp/routes/web.php
+++ anthony-testApp/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -34,6 +35,7 @@ Route::get('/test', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/home', [HomeController::class,'index']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
