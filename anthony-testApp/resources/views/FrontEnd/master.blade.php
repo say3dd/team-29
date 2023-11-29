4224fdc232c7d7bf682d67f5f040562604a72fe7
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-11-29 21:56:41 +0000

    difficult

--- anthony-testApp/resources/views/FrontEnd/master.blade.php
+++ anthony-testApp/resources/views/FrontEnd/master.blade.php
@@ -37,7 +37,7 @@
                     @if (Route::has('login'))
 
                         @auth
-                            <a href="{{ url('/home') }}"
+                            <a href="{{ url('home') }}"
                                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                         @else
                             <a href="{{ route('login') }}"
