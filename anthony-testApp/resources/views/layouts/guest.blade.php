ee222ed8caf7e7f13db792213ea2ac743aaab030
BM786 <220065665@aston.ac.uk>
2023-11-29 10:26:47 +0000

    reverted with darker background

--- anthony-testApp/resources/views/layouts/guest.blade.php
+++ anthony-testApp/resources/views/layouts/guest.blade.php
@@ -22,7 +22,7 @@
                 </a> 
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-purple-700 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-purple-900 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
