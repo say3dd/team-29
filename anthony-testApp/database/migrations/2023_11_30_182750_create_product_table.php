591757501e2ddb9f63b4727ac09c3bac45d462ca
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-11-30 18:29:29 +0000

    product migration

--- anthony-testApp/database/migrations/2023_11_30_182750_create_product_table.php
+++ anthony-testApp/database/migrations/2023_11_30_182750_create_product_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('product', function (Blueprint $table) {
             $table->id();
+            $table->string('product-name');
+            $table->text('product-description');
+            $table->string('photp');
+            $table->decimal("price", 6, 2);
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('product');
     }
 };
