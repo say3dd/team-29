8e98dca2f95e5fc6d73f2e1ed271fe2325c6bd71
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-12-05 14:18:09 +0000

    dashboard fixed

--- anthony-testApp/routes/web.php
+++ anthony-testApp/routes/web.php
@@ -22,11 +22,10 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
 
 Route::get('/', function () {
-    return view('FrontEnd/home');
-});
+    return view('FrontEnd/home'); })->name('index');
 
 
 Route::get('/contactUs', function () {
