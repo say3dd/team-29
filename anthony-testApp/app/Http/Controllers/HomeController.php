0a4bfc33f74cb0cdeb9ebe41cbc61e45c0f6d8d5
say3dd <say3dm@hotmail.com>
2023-12-06 10:05:30 +0000

    removed unnecessary break statements

--- anthony-testApp/app/Http/Controllers/HomeController.php
+++ anthony-testApp/app/Http/Controllers/HomeController.php
@@ -22,11 +22,9 @@ class HomeController extends Controller
         switch($usertype){
             case 'admin':
                 return view('Admin.adminDashboard');
-                break;
 
             case 'user':
                 return view('dashboard');
-                break;
 
             default:
                 return redirect()->back();
