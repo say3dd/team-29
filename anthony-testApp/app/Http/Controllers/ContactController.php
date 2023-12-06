be49d0404d09d29711104e6fcc507ec0637b5749
BM786 <220065665@aston.ac.uk>
2023-12-06 15:15:15 +0000

    c

---
+++ anthony-testApp/app/Http/Controllers/ContactController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function showForm()
+    {
+        return view('contact.form');
+    }
+
+    public function submitForm(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'message' => 'required|string',
+        ]);
+        return redirect()->route('contact.show')->with('success', 'Your message has been sent successfully!');
+    }
+}
--- anthony-testApp/resources/views/FrontEnd/contactUs.blade.php
+++ anthony-testApp/resources/views/FrontEnd/contactUs.blade.php
@@ -4,5 +4,24 @@
 
 @section('content')
 <h1 class="text-white">ContactUs</h1>
+@if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    <form action="{{ route('contact.submit') }}" method="post">
+        @csrf
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" required>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required>
+
+        <label for="message">Message:</label>
+        <textarea name="message" id="message" rows="4" required></textarea>
+
+        <button type="submit">Submit</button>
+    </form>
 
 @endsection
