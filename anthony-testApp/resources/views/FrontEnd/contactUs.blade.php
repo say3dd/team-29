717a85277787d472c190e9ce91405b04461f0cfb
BM786 <220065665@aston.ac.uk>
2023-12-06 10:38:50 +0000

    c1

--- anthony-testApp/resources/views/FrontEnd/contactUs.blade.php
+++ anthony-testApp/resources/views/FrontEnd/contactUs.blade.php
@@ -10,46 +10,5 @@
 
 @section('content')
 <h1 class="text-white">ContactUs</h1>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Contact Form') }}</div>
 
-                    <div class="card-body">
-                        @if (session('success'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('success') }}
-                            </div>
-                        @endif
-
-                        <form method="POST" action="{{ route('contact.submit') }}">
-                            @csrf
-
-                            <div class="form-group">
-                                <label for="name">{{ __('Name') }}</label>
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email">{{ __('E-Mail Address') }}</label>
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="message">{{ __('Message') }}</label>
-                                <textarea id="message" class="form-control" name="message" rows="4" required>{{ old('message') }}</textarea>
-                            </div>
-
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Message') }}
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
