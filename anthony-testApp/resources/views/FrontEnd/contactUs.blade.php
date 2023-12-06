40403b897ab2b98e92bb42b28a2516fe8741a065
BM786 <220065665@aston.ac.uk>
2023-12-06 15:02:35 +0000

    b

--- anthony-testApp/resources/views/FrontEnd/contactUs.blade.php
+++ anthony-testApp/resources/views/FrontEnd/contactUs.blade.php
@@ -3,12 +3,6 @@
 @section('title', 'ContactUs')
 
 @section('content')
-
-<!-- resources/views/contact.blade.php -->
-
-@extends('layouts.app')
-
-@section('content')
 <h1 class="text-white">ContactUs</h1>
 
 @endsection
