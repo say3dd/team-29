81c3dd4161318500c6f2d2a20b5417c3fcc0bc1e
noramknarf <45110161+noramknarf@users.noreply.github.com>
2023-12-07 16:11:13 +0000

    basket delete (still includes debug code)

--- anthony-testApp/resources/views/FrontEnd/basket.blade.php
+++ anthony-testApp/resources/views/FrontEnd/basket.blade.php
@@ -2,9 +2,15 @@
     <h1>This is the basket page</h1>
     <h2>Front-end can make it pretty once functionality is sorted</h2>
     <h2>Below here should be the user ID:</h2>
-    <h1>{{$userID}}</h1>
+    @if(isset($del))
+    <h1>{{$del}}</h1>
+    @endif
     @foreach($userBasket as $b)
-        <h1>{{$b->id}}, {{$b->created_at}}</h1>
-        <h1>test</h1>
+        <h1>{{$b->product_name}}, {{$b->product_price}}</h1>
+        <form action='{{route('basket.remove')}}' method='post'>
+            @csrf
+            <input type="hidden" name="basketToDelete" value={{$b->id}}>
+            <button class="button_cart_laptop"> Remove from basket </button>
+        </form>
     @endforeach
 </div>
