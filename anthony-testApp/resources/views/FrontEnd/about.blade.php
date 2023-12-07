125c443264dac52ce6d81a06c8fcc85f099928e4
Anthony <147273551+AnthonyResuello@users.noreply.github.com>
2023-12-07 11:20:28 +0000

    Created about page template

---
+++ anthony-testApp/resources/views/FrontEnd/about.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valhalla</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/about_style.css') }}">
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
+</head>
+
+<body>
+    <!--             Header                      -->
+
+    <header>
+        @include('header')
+   
+
+            <!-- About Section-->
+
+    <!--            Footer                      -->
+ 
+        @include('footer')
+    
+       
+   
+        
+</body>
+
+</html>
