1fa6a783b0731a194deebac7cac2fc4c22db55e6
Elizaveta Mikheeva <230067459@aston.ac.uk>
2023-12-07 00:23:15 +0000

    product_responsivness

--- anthony-testApp/resources/views/product/product.blade.php
+++ anthony-testApp/resources/views/product/product.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Product</title>
-    <link rel="stylesheet" href="{{asset('assets/css/style_sheet.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/style_sheet_Product_Template.css')}}">
 </head>
 
     @include ('header')
