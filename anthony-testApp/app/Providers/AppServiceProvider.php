380063033c500cecc5b6ce8545c52d6cee678648
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-12-08 13:47:49 +0000

    FrontEnd.home instead of master (testing purposes only).

--- anthony-testApp/app/Providers/AppServiceProvider.php
+++ anthony-testApp/app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        View::composer('FrontEnd.master', function ($view) {
+        View::composer('FrontEnd.home', function ($view) {
             $bestSellerLaptops = Product::whereIn('product_id', [3, 2, 10])->orderBy('product_id')->get();  
             $view->with('bestSellerLaptops', $bestSellerLaptops);
         });
