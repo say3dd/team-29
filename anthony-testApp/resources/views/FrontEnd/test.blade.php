4eb5141bd04418ea94f906ad57a8b05faeb67dfd
Elizaveta Mikheeva <230067459@aston.ac.uk>
2023-12-08 07:08:52 +0000

    responsivness test

--- anthony-testApp/resources/views/FrontEnd/test.blade.php
+++ anthony-testApp/resources/views/FrontEnd/test.blade.php
@@ -6,42 +6,83 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Product</title>
-    <link rel="stylesheet" href="{{asset('assets/css/style_sheet_Product_template.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/style_sheet_2.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
 </head>
 
  @include ('header')
 
 <body>
-    <h1>
-        <div class="background_shape1"></div>
-        <div class="background_shape_1_2"></div>
+<div class="main">
+    {{-- Section 1 starts here --}}
+        <div class="background_shape1">
+            <div class="background_shape_1_2">
+            <div class="slideshow-container">
+            <div class="slide">
+                <div class="mySlides">
+                    <img class="slide_image" src="{{asset('assets/images_product/laptop4.jpg')}}" style=" " alt="">
+                </div>
+                <div class="mySlides">
+                    <img class="slide_image" src="{{asset('assets/images_product/laptop2.jpg')}}" alt="">
+                </div>
+                <div class="mySlides">
+                    <img class="slide_image" src="{{asset('assets/images_product/laptop3.jpg')}}" alt="">
+                </div>
+                <div class="mySlides">
+                    <img class="slide_image" src="{{asset('assets/images_product/laptop1.jpg')}}" alt="">
+                </div>
+                <div class="mySlides">
+                    <img class="slide_image" src="{{asset('assets/images_product/laptop5.jpg')}}" alt="">
+                </div>
+                <button class="previousSlide" onclick="plusDivs(-1)">&#10094;</button>
+                <button class="nextSlide" onclick="plusDivs(1)">&#10095;</button>
+            </div>
+            <script>
+                var slideIndex = 1;
+                showDivs(slideIndex);
+
+                function plusDivs(n) {
+                    showDivs(slideIndex += n);
+                }
+
+                function showDivs(n) {
+                    var i;
+                    var x = document.getElementsByClassName("mySlides");
+                    if (n > x.length) { slideIndex = 1 }
+                    if (n < 1) { slideIndex = x.length }
+                    for (i = 0; i < x.length; i++) {
+                        x[i].style.display = "none";
+                    }
+                    x[slideIndex - 1].style.display = "block";
+                }
+            </script>
+            </div>
+        </div>
+        <div class="product_information">
         <p class="product_name">
             Dell Alienware x16 Gaming Laptop</p>
-
-        <p class="line1">
-            ____________________________</p>
-
-        <p class="product_main_features">
-            &nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;13th Gen Intel Core i9-13900HK<br>
-            &nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;Windows 11 Home<br>
-            &nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;NVIDIA® GeForce RTX 4090<br>
-            &nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;16", QHD+ 2560x1600, 240Hz<br>
-            &nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;32 GB 6000 MT/s RAM, 1 TB SSD<br><br>
-        </p>
-        <p class="line2">
-            ____________________________
-        </p>
-        <p class="product_price">
-            Price:£3,349.00
-        </p>
-        <p class="product_price_no_VAT">
-            £2,857.50
+        <hr><br>
+        <div class = "product_main_features">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;13th Gen Intel Core i9-13900HK<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;Windows 11 Home<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;NVIDIA® GeForce RTX 4090<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;16", QHD+ 2560x1600, 240Hz<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;32 GB 6000 MT/s RAM, 1 TB SSD<br>
+        </div>
+        <div class="button_view_specification">
+            <button class="button_view_specification" onclick="openMessageBox()">
+                View all specifications </button>
+        </div><br>
+        <hr><br>
+        <div class = "product_price">
+            &nbsp;&nbsp;&nbsp;&nbsp;Price:£3,349.00
+        </div><br>
+        <div class = "product_price_no_VAT">
+             &nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;£2,857.50
             excluding VAT @20%
-        </p>
-        <p class="line3">
-            ____________________________
-        </p>
+        </div>
+        <br><hr>
+        <div class="buttons">
         <button class="button_cart">
             <img src="{{asset('assets/images_product/icon_basket.png')}}" alt="" width="20" height="25">
             Add to Basket
@@ -51,12 +92,7 @@
             Add to Wish List
         </button>
         </div>
-
-        <div class="button_view_specification">
-            <button class="button_view_specification" onclick="openMessageBox()"> View
-                all specifications </button>
-        </div>
-
+        {{-- Part of Section 1, for showcasing all of the specification of the product --}}
         <div id="myModal" class="modal">
             <div class="modal-content">
                 <span class="close" onclick="closeMessageBox()">&times;</span>
@@ -180,47 +216,9 @@
                     </p>
                 </div>
             </div>
+
         </div>
 
-        <div class="slideshow-container">
-            <div class="slide">
-                <div class="mySlides">
-                    <img class="slide_image" src="{{asset('assets/images_product/laptop4.jpg')}}" style=" " alt="">
-                </div>
-                <div class="mySlides">
-                    <img class="slide_image" src="{{asset('assets/images_product/laptop2.jpg')}}" alt="">
-                </div>
-                <div class="mySlides">
-                    <img class="slide_image" src="{{asset('assets/images_product/laptop3.jpg')}}" alt="">
-                </div>
-                <div class="mySlides">
-                    <img class="slide_image" src="{{asset('assets/images_product/laptop1.jpg')}}" alt="">
-                </div>
-                <div class="mySlides">
-                    <img class="slide_image" src="{{asset('assets/images_product/laptop5.jpg')}}" alt="">
-                </div>
-                <button class="previousSlide" onclick="plusDivs(-1)">&#10094;</button>
-                <button class="nextSlide" onclick="plusDivs(1)">&#10095;</button>
-            </div>
-            <script>
-                var slideIndex = 1;
-                showDivs(slideIndex);
-
-                function plusDivs(n) {
-                    showDivs(slideIndex += n);
-                }
-
-                function showDivs(n) {
-                    var i;
-                    var x = document.getElementsByClassName("mySlides");
-                    if (n > x.length) { slideIndex = 1 }
-                    if (n < 1) { slideIndex = x.length }
-                    for (i = 0; i < x.length; i++) {
-                        x[i].style.display = "none";
-                    }
-                    x[slideIndex - 1].style.display = "block";
-                }
-            </script>
         </div>
         </div>
         <script>
@@ -235,81 +233,82 @@
                 modal.style.display = "none";
             }
         </script>
-    </h1>
-    <h2>
-        <div class="background_shape2"></div>
-        <div class="laptop_image_spec">
-            <img class="unique_feature1_image" src="{{asset('assets/images_product/laptop_specification1.jpg')}}">
-            <img class="unique_feature2_image" src="{{asset('assets/images_product/laptop_specification2.jpg')}}">
-            <img class="unique_feature3_image" src="{{asset('assets/images_product/laptop_specification3.jpg')}}">
-
-            <p class="unique_feature1">
-                Micro-LED stadium lighting
-            </p>
-            <p class="unique_feature2">
-                Per-key lighting
-            </p>
-            <p class="unique_feature3 ">
-
-                AlienFX touchpad
-            </p>
-
-            <p class="unique_feature1_content">
+        </div>
+    {{-- Section 1 ends here --}}
+    {{-- Section 2 ends here --}}
+    <div class = "container_section2">
+        <div class="features_images">
+            <img  src="{{asset('assets/images_product/laptop_specification1.jpg')}}">
+            <img  src="{{asset('assets/images_product/laptop_specification2.jpg')}}">
+            <img  src="{{asset('assets/images_product/laptop_specification3.jpg')}}">
+        </div>
+        <div class="unique_features">
+            
+                <div class="unique_feature1">Micro-LED stadium lighting</div>
+                <div class="unique_feature2">Per-key lighting</div>
+                <div class="unique_feature3">AlienFX touchpad</div>
+            
+        </div>
+        <div class = "unique_features_content">
+            <div class="unique_feature1_content">
                 Express yourself with bold,
                 iconic lightning featuring 100
                 micro-LED lights for an
                 instantly captivating, bright
                 and fluid look.
-            </p>
-            <p class="unique_feature2_content">
+            </div>
+            <div class="unique_feature2_content">
                 Customise your gaming
                 experience with nearly infinite
                 colour effects and
                 combinations via the
                 Alienware Command Center.
-            </p>
-            <p class="unique_feature3_content">
+            </div>
+            <div class="unique_feature3_content">
                 Our multi-touch AlienFX
                 touchpad with integrated
                 scrolling and customisable
                 lighting is back, and 15.5%
                 larger in area than the x15’s
                 touchpad.
-            </p>
+            </div>
         </div>
-    </h2>
-    <h3>
-        <div class="background_shape3">
-            <p class="unique_feature4">
+    </div>
+    {{-- Section 2 ends here --}}
+    {{-- Section 3 begins here --}}
+        <div class="container_section3">
+            <p class="unique_feature_title_title">
                 Total Command </p>
-            <p class="unique_feature4_content">
+            <p class="unique_feature_content">
                 Control more than ever with the all-new Alienware Command Center.</p>
-            <p class="unique_feature5">
+            <p class="unique_feature_title">
                 Presets and overclocking</p>
-            <p class="unique_feature5_content">
+            <p class="unique_feature_content">
                 Maximise your gameplay with performance presets while overclocking capabilities give you the speed you
                 need.
             </p>
-            <p class="unique_feature6">
+            <p class="unique_feature_title">
                 Performance overlay</p>
-            <p class="unique_feature6_content">
+            <p class="unique_feature_content">
                 Manage and see CPU, GPU, memory and thermals stats without exiting the game.
             </p>
-            <p class="unique_feature7">
+            <p class="unique_feature_title">
                 AlienFX</p>
-            <p class="unique_feature7_content">
+            <p class="unique_feature_content">
                 Customise your set up with lighting settings that extend to all your Alienware accessories, as well as
                 keybinds and
                 calibration.
             </p>
-            <p class="unique_feature8">
+            <p class="unique_feature_title">
                 AlienVision</p>
-            <p class="unique_feature8_content">
+            <p class="unique_feature_content">
                 Toggle monitor-based overlays on/off to help you focus on the targets at hand.
             </p>
             <img class="unique_feature4_5_6_7_8_image" src="{{asset('assets/images_product/laptop_specification5.jpg')}}">
         </div>
-        <div class="background_shape4">
+    {{-- Section 3 ends here --}}
+    {{-- Section 4 begins here --}}
+        <div class="container_section4">
             <img class="unique_feature9_image" src="{{asset('assets/images_product/laptop_specification4.jpg')}}">
             <p class="unique_feature9">
                 Seamless Gaming
@@ -323,10 +322,11 @@
                 game.
             </p>
         </div>
-    </h3>
-    <h4>
-        <p class="title_related_products"> Related Products </p>
-        <div class="background_shape5">
+    {{-- Section 4 ends here --}}
+    {{-- Section 5 begins here --}}
+
+        <div class="container_section5">
+            <div class="title_related_products"> Related Products </div>
             <div class="laptop1">
                 <img class="related_product_image" src="{{asset('assets/laptop_images/MSI Titan GT77 HX 13V/msi_Titan_GT77_HX_13V.jpg')}} ">
                 <button class="related_product_content">MSI Titan GT77 HX 13V</button>
@@ -335,7 +335,6 @@
                 <p> - 128GB RAM</p>
                 <button class="button_cart_laptop"> Add to Basket </button>
             </div>
-
             <div class="laptop1">
                 <img class="related_product_image" src="{{asset('assets/laptop_images/Alienware M16/alienware_m16.jpg')}}">
                 <button class="related_product_content">Alienware m16</button>
@@ -352,7 +351,6 @@
                 <p> - 128GB RAM</p>
                 <button class="button_cart_laptop"> Add to Basket </button>
             </div>
-
             <div class="laptop1">
                 <img class="related_product_image" src="{{asset('assets/laptop_images/Alienware M18/alienware-m18.jpg ')}}">
                 <button class="related_product_content">Alienware m18</button>
@@ -362,7 +360,7 @@
                 <button class="button_cart_laptop" style="margin-top: 0px;"> Add to Basket </button>
             </div>
         </div>
-    </h4>
+</div>
 </body>
 
 @include('footer')
