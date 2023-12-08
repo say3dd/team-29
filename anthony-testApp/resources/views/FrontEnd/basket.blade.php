a03b8c78d1f11c320fe0a621d928626c75ca16ec
noramknarf <45110161+noramknarf@users.noreply.github.com>
2023-12-08 10:50:34 +0000

    cleaning up debug code

--- anthony-testApp/resources/views/FrontEnd/basket.blade.php
+++ anthony-testApp/resources/views/FrontEnd/basket.blade.php
@@ -1,10 +1,7 @@
 <div>
     <h1>This is the basket page</h1>
     <h2>Front-end can make it pretty once functionality is sorted</h2>
-    <h2>Below here should be the user ID:</h2>
-    @if(isset($del))
-    <h1>{{$del}}</h1>
-    @endif
+
     @foreach($userBasket as $b)
         <h1>{{$b->product_name}}, {{$b->product_price}}</h1>
         <form action='{{route('basket.remove')}}' method='post'>
