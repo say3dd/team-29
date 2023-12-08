e4c8df5215e4c9555fbbe7b7aa0bd844309aaef5
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-12-08 15:01:35 +0000

    g

--- anthony-testApp/routes/web.php
+++ anthony-testApp/routes/web.php
@@ -48,7 +48,7 @@ Route::get('/test', function () {
 });
 
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
