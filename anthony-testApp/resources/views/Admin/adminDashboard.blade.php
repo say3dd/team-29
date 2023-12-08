e56e79078f511a0057440fb7af0c31953e62707d
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-12-08 10:53:41 +0000

    minor change test

--- anthony-testApp/resources/views/Admin/adminDashboard.blade.php
+++ anthony-testApp/resources/views/Admin/adminDashboard.blade.php
@@ -15,7 +15,7 @@
 
              {{-- admin navigation to product Lists --}}
                     <div class="admin-dashboard-nav" style="padding: 5px; margin: 5px;">
-                        <div class="p-5 m-5 mx-w-100% w-100% bg-cyan-200 rounded flex">
+                        <div class="p-5 m-5 mx-w-100% w-100% bg-cyan-200 rounded flex flex-col">
                         <span class=" p-5 m-5 text-center rounded bg-purple-300">
                         <a class= "text-center align-middle text-white" href="{{url('index')}}">Home</a>
                          </span>
