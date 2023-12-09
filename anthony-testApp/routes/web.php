877cab16af0a34f1d4fda87f1834eabc390ec207
BravoBoy2 <abubakar.siddik2146@gmail.com>
2023-12-09 14:33:46 +0000

    dashboard to home route fixed

--- anthony-testApp/routes/web.php
+++ anthony-testApp/routes/web.php
@@ -19,54 +19,48 @@ use App\Http\Controllers\ProfileController;
 */
 
 
-
-
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', function () {
+    return view('FrontEnd.landing');
+});
 
 Route::get('/test', function () {
     return view('FrontEnd/test');
 });
 
-Route::middleware('guest')->group(function () {
-
-    //code for index should be here
-    Route::get('/', function () {
-        return view('FrontEnd.landing');
-    });
-
-    // your code ends here
 
     Route::get('/index', [HomeController::class,'index'])->name('index');
-    Route::get('/contactUs',[ContactController::class, 'index'])->name('contactUs');;
+    Route::get('/contactUs',[ContactController::class,'contact'])->name('contactUs');;
 
     // Addded route function to the about page
     Route::get('/about', function (){return view('FrontEnd/about');})->name('about');
 
 
     // Route::get('/', );
-    Route::get('/product', [ProductController::class, 'showProducts'])->name('product');
-    Route::get('/product/{id}', [ProductController::class, 'show'])->name('laptops.show');
+    Route::get('/product', [ProductController::class, 'showProducts'])->middleware(['guest'])->name('product');
+    Route::get('/product/{id}', [ProductController::class, 'show'])->middleware(['guest'])->name('laptops.show');
 
-    Route::get('/contact', [ContactController::class, 'showForm'])->name('contact.show');
-    Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact.submit');
-});
+    Route::get('/contact', [ContactController::class, 'showForm'])->middleware(['guest'])->name('contact.show');
+    Route::post('/contact', [ContactController::class, 'submitForm'])->middleware(['guest'])->name('contact.submit');
 
 
-Route::middleware('auth', 'verified')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/home', [HomeController::class, 'authHome'])->name('home');
+    Route::get('/home', [HomeController::class,'authHome'])->name('home');
 });
 
-Route::middleware('auth', 'admin')->group(function () {
+Route::middleware('auth','admin')->group(function () {
 
     Route::get('/plist', function () {
         return view('Admin.ProductList');
     })->name('plist');
 });
+
+    Route::get('/index', [HomeController::class, 'index'])->name('index');
 
 
 require __DIR__ . '/auth.php';
