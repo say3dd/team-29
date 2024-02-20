f0475114ad545304ed5fc1b421b346e038eaa2e6
AstonDevice <abubakar.siddik2146@gmail.com>
2024-02-20 18:00:14 +0000

    App name has been changed to Valhalla

--- anthony-testApp/resources/views/FrontEnd/home.blade.php
+++
@@ -1,6 +0,0 @@
- <!-- 
-         _____________________________________ Created and designed the Homepage of the webiste by @AnthonyResuello (Anthony Resuello) ____________________________________________
--->
-@extends('FrontEnd.master')
-@section('title', 'Home')
-
