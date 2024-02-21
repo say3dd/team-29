a346fc4a9ea3946969f186fbe7eb902505327be9
KraeBM <bilal.businessacc@gmail.com>
2024-02-21 01:01:36 +0000

    re-commiting code

---
+++ anthony-testApp/storage/framework/views/57d7c579114ba140d88af26667ab7572.php
@@ -0,0 +1,31 @@
+<!-- @KraeBM (Bilal Mohamed) worked on the blade templating of this website  -->
+<!-- @ElizavetaMikheeva (Elizaveta Mikheeva) - implemented the front-end (design) of the Products webpage using CSS  -->
+
+<?php $__env->startSection('productP'); ?>
+
+<?php $__currentLoopData = $laptops; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $laptop): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+<div class="laptop_all">
+    <img class="image_all_laptop" src="<?php echo e(asset($laptop->image_path)); ?>">
+    <div class="laptop_all_text">
+        <a style= "color: inherit" href="<?php echo e(route('laptops.show', $laptop->product_id)); ?>"> <?php echo e($laptop->laptop_name); ?> </a>
+        <p><?php echo e($laptop->processor); ?></p>
+        <p><?php echo e($laptop->GPU); ?></p>
+    </div>
+    <p style="margin-bottom: 42px;">RAM: <?php echo e($laptop->RAM); ?> GB </p>
+    <p class="price" style=" font-weight: bold; margin-bottom: 0px; text-decoration: underline;
+    text-decoration:underline; text-decoration-color:aquamarine ">Price: £<?php echo e($laptop->price); ?></p>
+    <br>
+    
+    <form action='<?php echo e(route('product.getInfo')); ?>' method='post'>
+        <?php echo csrf_field(); ?>
+        <input type="hidden" name="laptopData" value=<?php echo e($laptop->product_id); ?>>
+        <button class="button_cart_laptop"> Add to Basket </button>
+    </form>
+    <!--It took me 8 hours of work just to get this thing to get this data and of course it comes out as a string I can't separate... -->
+    <!--productToAdd is the string of data that the function returns, what I need to do is get the id from this and use that to update the basket DB -->
+    
+
+</div>
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('productL', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\xampp\team-29\anthony-testApp\resources\views/Product_files/products2.blade.php ENDPATH**/ ?>
---
+++ anthony-testApp/storage/framework/views/e6964cc640b1f954273247460c2a7dca.php
@@ -0,0 +1,53 @@
+<!-- 
+         ______________________________   Created and designed the Header of the webiste by @AnthonyResuello (Anthony Resuello)     ____________________________________________
+
+    - Created and desgined the header to make it consistent for all pages of the website
+
+-->
+
+
+<header>
+            
+    <section class="nav-header">
+    
+        <a href="<?php echo e(route('index')); ?>" class="logo"> <img src="<?php echo e(asset('assets/images/Screenshot_2023-11-16_030651.png')); ?>" alt=""></a>
+        
+        <input type="checkbox" id="check">
+        <label for="check" class="menu-icon">
+            <i class="bx bx-menu" id="menu"></i>
+            <i class="bx bx-x" id="close"></i>
+        </label>
+        <nav class="navbar">
+            <a href="<?php echo e(route('index')); ?>">Home</a>
+            <a href="<?php echo e(route('productspage.id', ['id' => 1])); ?>">Products</a>
+            <a href="<?php echo e(route('about')); ?>">About</a>
+            <a href="<?php echo e(route('contactUs')); ?>">Contact Us</a>
+         
+
+            <!--        Fixed the heading so that the login and register is included in the header           -->
+
+            <?php if(Route::has('login')): ?>
+
+                <?php if(auth()->guard()->check()): ?>
+                    <a href="<?php echo e(url('home')); ?>"
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                <?php else: ?>
+                    <a href="<?php echo e(route('login')); ?>"
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"><i
+                            class="bx bx-user"></i> Log in</a>
+
+                    <?php if(Route::has('register')): ?>
+                        <a href="<?php echo e(route('register')); ?>"
+                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                    <?php endif; ?>
+                <?php endif; ?>
+            <?php endif; ?>
+
+            <!--<a href="#" class="login-text"><i class="bx bx-user"></i> Log in</a>  !-->
+            <a href="<?php echo e(route('basket')); ?>" class="cart-icon"><i class="bx bx-shopping-bag"></i> Basket</a>
+            
+        </nav>
+    </section>
+    <!--         Hero Section         -->
+   
+</header><?php /**PATH C:\xampp\htdocs\xampp\team-29\anthony-testApp\resources\views/header.blade.php ENDPATH**/ ?>
