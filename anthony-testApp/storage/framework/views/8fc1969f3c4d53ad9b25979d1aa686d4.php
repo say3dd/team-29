960c4f7ec70003eaa42aa1197d787a5d61cc8955
BM786 <220065665@aston.ac.uk>
2024-02-22 08:49:42 +0000

    tracking blade has been added

---
+++ anthony-testApp/storage/framework/views/8fc1969f3c4d53ad9b25979d1aa686d4.php
@@ -0,0 +1,8 @@
+ <!-- 
+         _____________________________________ Created and designed the Homepage of the webiste by @AnthonyResuello (Anthony Resuello) ____________________________________________
+-->
+
+<?php $__env->startSection('title', 'Home'); ?>
+
+
+<?php echo $__env->make('FrontEnd.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\aliba\Documents\Git\team-29\anthony-testApp\resources\views/FrontEnd/home.blade.php ENDPATH**/ ?>
