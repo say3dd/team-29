4f531509ed9e05b84d3ae0a8ad075fe21802f3ea
bilal <bilal.businessacc@gmail.com>
2024-02-23 15:23:42 +0000

    bilal test

--- anthony-testApp/storage/framework/views/a420d2231a76a81c28df7cec5b220bc6.php
+++
@@ -1,8 +0,0 @@
- <!-- 
-         _____________________________________ Created and designed the Homepage of the webiste by @AnthonyResuello (Anthony Resuello) ____________________________________________
--->
-
-<?php $__env->startSection('title', 'Home'); ?>
-
-
-<?php echo $__env->make('FrontEnd.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\xampp\team-29\anthony-testApp\resources\views/FrontEnd/home.blade.php ENDPATH**/ ?>
