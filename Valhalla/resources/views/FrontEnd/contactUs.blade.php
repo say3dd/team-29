bd1622611ace7fc0bbf93e3357c4ee358559bf5c
BM786 <220065665@aston.ac.uk>
2024-02-24 11:52:58 +0000

    Back button fixed on all 3 pages to make user actually go back instead of taking to index

--- Valhalla/resources/views/FrontEnd/contactUs.blade.php
+++ Valhalla/resources/views/FrontEnd/contactUs.blade.php
@@ -41,11 +41,15 @@ Author @BM786 Basit Ali Mohammad == worked on this page.
         </div>
     </form>
     <div class="items-center justify-start -mt-11 ml-2">
-        <a href="{{route('index')}}">
-        <x-primary-button>
+        <x-primary-button onclick="goBack()">
             {{ __('Back') }}
         </x-primary-button>
-    </a>
     </div>
+    
+    <script>
+        function goBack() {
+            window.history.back();
+        }
+    </script>
 </x-guest-layout>
 
