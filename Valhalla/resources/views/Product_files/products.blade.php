1b2b5aa4840eb2025f95233d363a718ed02128fd
KraeBM <bilal.businessacc@gmail.com>
2024-03-08 02:36:32 +0000

    Made the products page show the product specs within the products listed.

--- Valhalla/resources/views/Product_files/products.blade.php
+++ Valhalla/resources/views/Product_files/products.blade.php
@@ -11,10 +11,36 @@
     <div class="laptop_all_text">
         <!--@say3dd (Mohammed Miah) Routing to make the user go to the details of an individual product by clicking on the name -->
         <a style= "color: inherit" href="{{ route('products', $product->product_id) }}"> {{$product->product_name}} </a>
-{{--        <p>{{ $product->processor }}</p>--}}
-{{--        <p>{{ $product->GPU }}</p>--}}
+        {{-- Uses regex to find the specific pattern to find the exact data needed to be shown : here its the processor, gpu and ram--}}
+ @php
+        $description = $product->product_description;
+
+        // Regular expressions to find Processor, RAM, and GPU
+        $patterns = [
+            'Processor' => '/Processor:\s*([^\n]+)/',
+            'RAM' => '/RAM:\s*([^\n]+)/',
+            'GPU' => '/GPU:\s*([^\n]+)/',
+        ];
+// key refers to the descriptive names (processor,gpu etc), $pattern is the regular expressions, details is what stores the data found
+        $details = [];
+        foreach ($patterns as $key => $pattern) {
+            if (preg_match($pattern, $description, $matches)) {
+                //$matches[1] used for only the specified details matching the regex
+                $details[$key] = $matches[1];
+            }
+        }
+        @endphp
+        {{--If the details array contains the same string as 'whats needed to be found', then its displays it using <p></p>--}}
+        @if(isset($details['Processor']))
+            <p> {{ $details['Processor'] }}</p>
+        @endif
+        @if(isset($details['GPU']))
+            <p> {{ $details['GPU'] }}</p>
+        @endif
     </div>
-{{--    <p style="margin-bottom: 42px;">RAM: {{$product->RAM}} GB </p>--}}
+    @if(isset($details['RAM']))
+        <p>{{ $details['RAM'] }}</p>
+    @endif
     <p class="price" style=" font-weight: bold; margin-bottom: 0px; text-decoration: underline;
     text-decoration:underline; text-decoration-color:aquamarine ">Price: £{{ $product->price }}</p>
     <br>
