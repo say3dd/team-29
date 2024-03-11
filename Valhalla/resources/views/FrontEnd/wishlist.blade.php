38580b060e20ab6aaff29a463a6604f34d797874
BM786 <220065665@aston.ac.uk>
2024-03-11 17:39:52 +0000

    Revert "wishlist page styling"
    This reverts commit 531d68729f360ea1185bda77ec700a8f83de85da.

--- Valhalla/resources/views/FrontEnd/wishlist.blade.php
+++ Valhalla/resources/views/FrontEnd/wishlist.blade.php
@@ -3,10 +3,7 @@
 <x-app-layout>
   <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <style>
-    .bg-indigo-950 {
-      width: 100%;
-      height: 100vh;
-    }
+
     #sortable { 
       list-style-type: none; 
       margin: 0; 
@@ -34,25 +31,17 @@
     }
     .sortable-placeholder { height: 120px; }
 
-    .custom-container {
-    width: 80%;
-    margin: auto;
-    position: absolute;
-    top: 185px; 
-    left: 148px;
-  }
+    p {
+      color:#020202;
+    }
 
-    #home {
-      position: absolute;
-      top: 290px; 
-      left: 148px; 
+    .class2 {
+      background-color: #1A3A8A; 
     }
-   
-    #save-wishlist {
-      position: absolute;
-      bottom: -20px; 
-      right: 148px; 
+    .class2 p {
+      color: #020202; 
     }
+
   </style>
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -90,23 +79,23 @@ $(document).ready(function() {
 </head>
 
 <body>
-    <div class="py-6 bg-white">
+  <class>
+    <div class=" py-6 bg-white">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class= "overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-purple-700 overflow-hidden shadow-sm sm:rounded-lg">
             </div>
   <h2 class="font-semibold text-xl text-gray-800 leading-tight">
     {{ __('Wishlist') }}
 </h2>
         </div>
     </div>
+</class>
+</body>
 
-
-<div class=" bg-indigo-950">
+<class2 class="class2">
   @if($wishlistItems->isEmpty())
-    <div class="p-6 text-white bg-purple-700 overflow-hidden shadow-sm sm:rounded-lg custom-container">
-        {{ __("Your wishlist is empty. Return home here:") }}
-    </div>
-  <x-primary-button class=" border-white" id="home"> <a href="{{ route('index') }}">Home</a> </x-primary-button>
+  <p>Your wishlist is empty. Return home here: </p> 
+  <x-primary-button> <a href="{{ route('index') }}">Home</a> </x-primary-button>
   @else
   <div id="sortable">
     @foreach($wishlistItems as $product)
@@ -128,9 +117,9 @@ $(document).ready(function() {
 
 
     @endforeach
-    @endif
-  <x-primary-button class="border-white" id="save-wishlist">Save Wishlist</x-primary-button>
+  </div>
+  @endif
+  <x-primary-button id="save-wishlist">Save Wishlist</x-primary-button>
                         </div>
-                      </div>
-                    </div>
+</class2>
 </x-app-layout>
