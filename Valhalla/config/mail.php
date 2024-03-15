584101abfd4da16d8965b3564bda321a2144a730
AstonDevice <abubakar.siddik2146@gmail.com>
2024-03-15 17:45:29 +0000

    new "forgot password" function has been implemented

--- Valhalla/config/mail.php
+++ Valhalla/config/mail.php
@@ -100,8 +100,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'cs2tpteam29@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Valhalla'),
     ],
 
     /*
