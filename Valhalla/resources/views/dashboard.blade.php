083c2b3b20743ce0cf9207f0acd31693942a7ad1
say3dd <say3dm@hotmail.com>
2024-03-20 15:25:41 +0000

    Add tracking functionality

--- Valhalla/resources/views/dashboard.blade.php
+++ Valhalla/resources/views/dashboard.blade.php
@@ -68,7 +68,7 @@
               <td class="px-4 py-2 border-gray-800 border bg-gray-200"> {{$item->tracking_number}} </td>
               <td class="px-4 py-2 border-gray-800 border bg-gray-200"> {{$item->price}} </td>
               <td class="px-4 py-2 border-gray-800 border bg-gray-200"> {{$item->created_at}} </td> 
-              <td class="px-4 py-2 border-gray-800 border bg-gray-200"><a href="{{route('tracking')}}">
+              <td class="px-4 py-2 border-gray-800 border bg-gray-200"><a href="{{ route('tracking', ['id' => $item->id]) }}">
                 <x-primary-button>
                     {{ __('Track') }}
                 </x-primary-button>
