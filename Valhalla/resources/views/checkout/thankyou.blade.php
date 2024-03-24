5c6a3f8b09d71a20b28ed4a217f3d8b0d6ea9401
Abubakar <abubakar.siddik2146@gmail.com>
2024-03-24 13:38:56 +0000

    some styling

--- Valhalla/resources/views/checkout/thankyou.blade.php
+++ Valhalla/resources/views/checkout/thankyou.blade.php
@@ -1,11 +1,16 @@
 <!--@noramknarf (Francis Moran) - linked back to index-->
 {{--
-    
+
 Author @BM786 Basit Ali Mohammad == worked on this page.
 
 --}}
-<div class="bg-white to-blue-300 mx-5 w-50% mx-w-100% h-2">
-<p><strong>Thank you!</strong> for you order! <br> 
-    Click below to return to the home page.</p>
-    <a href='{{route('index')}}'>return to home</a>
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<div class="bg-purple-900 w-full h-full p-3 m-3 flex align-middle justify-center items-center">
+<div class="bg-purple-600 text-white p-10 rounded-md mx-auto w-50% md:w-auto">
+    <p class="font-bold text-2xl">Thank you! for your order! <br>
+        Click below to return to the home page.</p>
+    <a href="{{ route('index') }}" class="text-blue-200 hover:text-blue-100 underline">return to home</a>
+</div>
 </div>
