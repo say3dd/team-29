1b4ceb3fe7ad7c0ffaa3428c88da9533dc9f5654
Abubakar <abubakar.siddik2146@gmail.com>
2024-03-25 08:42:26 +0000

    custom error blade styling

--- Valhalla/resources/views/_errors.blade.php
+++ Valhalla/resources/views/_errors.blade.php
@@ -2,7 +2,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<div class="block p-3 w-1/2 bg-red-500">
+<div class="block p-2 w-1/2 bg-red-500 align-middle rounded-md">
     <b>There are some errors in your submission:</b>
     @foreach ($errors->all() as $error)
         <p>{{ $error }}</p>
