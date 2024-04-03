7302118395f56e12de74801da29fa47e5d45497a
abubakar <abubakar.siddik2146@gmail.com>
2024-04-03 06:44:28 +0100

    new test

--- Valhalla/resources/views/welcome.blade.php
+++ Valhalla/resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
         </style>
     </head>
     <body class="antialiased">
-        {{-- testing git --}}
+
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
